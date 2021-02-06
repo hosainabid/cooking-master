67b3c317a16238ee9d580df82e5b5eff52c19407
Abid Hossain <hosainabid@gmail.com>
2021-02-07 03:04:42 +0600

    Complete HTML Code

---
+++ app.js
@@ -0,0 +1,11 @@
+// const searchBtn = document.getElementById('search-btn');
+// const mealList = document.getElementById('meal');
+// const mealDetailsContent = document.querySelector('.meal-details-content');
+// const recipeCloseBtn = document.getElementById('recipe-close-btn');
+
+// searchBtn.addEventListener('click', getMealList);
+
+
+// fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+// .then(res => res.json())
+// .then(data => console.log(data))
