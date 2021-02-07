c3c1bb624b617707e8db5c8dd825cb2342aeecb4
Abid Hossain <hosainabid@gmail.com>
2021-02-07 16:01:32 +0600

    JSON API Development

--- app.js
+++ app.js
@@ -1,11 +1,70 @@
-// const searchBtn = document.getElementById('search-btn');
-// const mealList = document.getElementById('meal');
-// const mealDetailsContent = document.querySelector('.meal-details-content');
-// const recipeCloseBtn = document.getElementById('recipe-close-btn');
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('.meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+const mealDetailsCss = document.querySelector('.meal-details');
 
-// searchBtn.addEventListener('click', getMealList);
+searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', getMealRecipe);
+recipeCloseBtn.addEventListener('click', () => {
+    mealDetailsContent.parentElement.classList.remove("showIngredient");
+    mealDetailsCss.style.display = "none";
+});
 
+function getMealList(){
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${searchInputTxt}`)
+    .then(response => response.json())
+    .then(data => {
+        let html = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                html += `
+                    <div class="meal-item" data-id="${meal.idMeal}">
+                        <div class="meal-img">
+                            <img src="${meal.strMealThumb}" alt="" srcset="">
+                        </div>
+                        <div class="meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href="#" class="recipe-btn">Get Ingredient</a>
+                        </div>
+                    </div>
+                `;
+            });
+        } 
+        mealList.innerHTML = html;
+    });
+};
 
-// fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
-// .then(res => res.json())
-// .then(data => console.log(data))
+function getMealRecipe(event){
+    event.preventDefault();
+    if(event.target.classList.contains('recipe-btn')){
+        let mealItem = event.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal(data.meals));
+    }
+}
+
+function mealRecipeModal(meal){
+    console.log(meal);
+    meal = meal[0];
+    let html = `
+            <div class="recipe-meal-img">
+                <img src="${meal.strMealThumb}" alt="">
+            </div>
+            <h2 class="recipe-title">${meal.strMeal}</h2>
+            <h3>Ingredients</h3>
+            <div class="ingredient_names">
+                <ul>
+                    <li>${meal.strIngredient1}</li>
+                    <li>${meal.strIngredient2}</li>
+                    <li>${meal.strIngredient3}</li>
+                    <li>${meal.strIngredient4}</li>
+                    <li>${meal.strIngredient5}</li>
+                </ul>
+            </div>
+        `;
+    mealDetailsContent.innerHTML = html;
+    mealDetailsContent.parentElement.classList.add('showIngredient');
+}
